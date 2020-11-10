8c66a30718aff24fbe01034c68eadcca0b5e3f62
naofumi suda <developsuda@gmail.com>
2020-11-10 22:47:15 +0900

    2020/11/10

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const http = require('http');
+const fs = require('fs');
+const ejs = require('ejs');
+ 
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
 
-var server = http.createServer(
-  (request,response)=>{
-    response.end('Hello Node.js!');
-  }
-);
+var server = http.createServer(getFromClient);
+
 server.listen(3000);
+console.log('Server start!');
+
+// ここまでメインプログラム==========
+
+// createServerの処理
+// function getFromClient(request, response){
+//   var content = ejs.render(index_page);
+//   response.writeHead(200, {'Content-Type': 'text/html'});
+//   response.write(content);
+//   response.end();
+// }
+
+function getFromClient(request, response) {
+  var content = ejs.render(index_page, {
+    title: "Indexページ",
+    content: "これはテンプレートを使ったサンプルページです。",
+  });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(content);
+  response.end();
+}
